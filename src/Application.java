576f9a4517cd849c10fe8df82323eab20625a498
sureshdtrainer <suresh.d.trainer@gmail.com>
2025-08-14 04:58:32 +0000

    Arrays

--- src/Application.java
+++ src/Application.java
@@ -68,7 +68,7 @@ public class Application {
 
         //Switch
 
-        String text = "status";
+       /* String text = "status";
         switch (text){
             case "start":
                 System.out.println("Machine started!!!");
@@ -82,11 +82,56 @@ public class Application {
                 System.out.println("Not a recoginized Command");
         }
 
+*/
 
+        //Arrays - group of like typed variables
+        int value = 7;
+        int[] values; //declaration
+        values = new int[3]; // assign the memory size
 
+        values[0] = 10; //assign
+        values[1] = 20;
+        values[2] = 30;
 
+        for (int i=0; i< values.length; i++){
+            System.out.println(values[i]);
+        }
+        // 1D
+        int[] numbers = {5, 6, 7 , 8 , 9}; //declare and initialize the array
+        for (int i=0; i< numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+        //Array of Strings
+        String[]  words = new String[3];
+        words[0] = "Hello";
+        words[1] = "to";
+        //words[2] = "Siemens"; // if not assigned the value will have null as value
 
+        for(String word: words){
+            System.out.println(word);
+        }
 
+        String[] fruits = {"apple", "banana", "pear", "kiwi"};
+        for(String fruit: fruits){
+            System.out.println(fruit);
+        }
 
+        //2D
+        int[] [] grid = {
+                {2, 4, 5},
+                {4, 7, 8},
+                {12, 23, 14, 15}
+        };
+        //Need 2 indices or index to access the values
+        System.out.println("value 1 1 : "+ grid[1] [1]);
+        System.out.println("value 0 2 : "+ grid[0] [2]);
+        System.out.println("value 2 3 : "+ grid[2] [3]);
+
+        for(int row = 0; row < grid.length; row++){
+            for(int col= 0; col<grid[row].length; col++){
+                System.out.print(grid[row][col]+"\t");
+            }
+            System.out.println();
+        }
     }
 }
